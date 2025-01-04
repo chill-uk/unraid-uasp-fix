51eeeac0a30b081f0890d9daae87cb3c8e0996e1
Christopher Hill <chill.signup@gmail.com>
2025-01-04 10:59:45 +0100

    feature/updatedHddInfoScript

--- unraid/boot/config/custom_scripts/get_hdd_info.sh
+++ unraid/boot/config/custom_scripts/get_hdd_info.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-# Function to strip partition number from device name
-strip_partition() {
-    echo "$1" | sed -E 's/([[:alpha:]/]+)([0-9]+)$/\1/'
-}
+# Extract and format the model number and serial number using smartctl
+model=$(smartctl -i "$devnode" | awk -F ': ' '/Device Model/ {print $2}')
+serial=$(smartctl -i "$devnode" | awk -F ': ' '/Serial Number/ {print $2}')
 
-# Log for debugging purposes
-echo "Debug: Running get_hdd_info.sh for $1" >> /tmp/udev_debug.log
-
-# Strip partition number if present
-DEVICE=$(strip_partition "$1")
-echo "Debug: Using device $DEVICE" >> /tmp/udev_debug.log
-
-# Extract Serial Number
-serial=$(hdparm -I "$DEVICE" | grep -i "Serial Number" | awk '{print $NF}')
-echo "Debug: Serial number found: $serial" >> /tmp/udev_debug.log
-
-# Check if serial and model were found
-if [ -z "$serial" ]; then
-    echo "Debug: Failed to extract serial for $DEVICE" >> /tmp/udev_debug.log
-    exit 1
+# If the model or serial number are emtpty, try with hdparm
+if [ -z "$model" ] || [ -z "$serial" ]; then
+    model=$(hdparm -I "$devnode" | awk -F ': ' '/Model Number/ {print $2}')
+    serial=$(hdparm -I "$devnode" | awk -F ': ' '/Serial Number/ {print $2}')
 fi
 
-# Output the values to be used by udev
-echo "SERIAL=TOSHIBA_MG08ACA16TE_$serial"
+# If the model or serial number are still empty, write an error message to the log
+if [ -z "$model" ] || [ -z "$serial" ]; then
+    echo "Error: Unable to retrieve model and serial number for $devnode" >> /var/log/syslog
+fi
 
-echo "Debug: Script completed successfully for $DEVICE" >> /tmp/udev_debug.log
+# If the model and serial were found, write them to the log and continue
+if [ -n "$model" ] && [ -n "$serial" ]; then
+
+    # Trim leading and trailing whitespace from model and serial
+    model=$(echo "$model" | sed 's/^[ \t]*//;s/[ \t]*$//')
+    serial=$(echo "$serial" | sed 's/^[ \t]*//;s/[ \t]*$//')
+
+    # Replace spaces in the model number with underscores
+    model=$(echo "$model" | sed 's/ /_/g')
+
+    # Combine the formatted model number and serial number
+    combined_info="ID_SERIAL=${model}_${serial}"
+
+    echo "DevNode: $devnode, Model: $model, Serial: $serial" >> /var/log/syslog
+    echo "DevNode: $devnode, Combined_info: $combined_info" >> /var/log/syslog
+    # Output the combined information
+    echo "$combined_info"
+fi
