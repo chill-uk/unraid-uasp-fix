a2645127f51ce00afd8b9bebfb12585eb5b9eb81
Christopher Hill <chill.signup@gmail.com>
2025-01-05 01:52:30 +0100

    chore/movedToCurl

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 cd /boot/config
 # check if go.orig exists, if not, backup go
 [ -f go.orig ] || mv go go.orig
-wget -qO- "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/go" > go
+curl -s -o go "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/go"
 
 # if the udev/rules.d directory doesn't exist, create it
 [ -d custom_scripts ] || mkdir -p custom_scripts
-wget -qO- "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/custom_scripts_get_hdd_info.sh" > /boot/config/custom_scripts/get_hdd_info.sh
+curl -s -o custom_scripts/get_hdd_info.sh "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/custom_scripts_get_hdd_info.sh"
 
 # if the udev/rules.d directory doesn't exist, create it
 [ -d rules.d ] || mkdir -p rules.d
-wget -qO- "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/rules.d/60-persistent-storage.rules" > /boot/config/rules.d/60-persistent-storage.rules
+curl -s -o rules.d/60-persistent-storage.rules "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/rules.d/60-persistent-storage.rules"
