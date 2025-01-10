463b0aea322291ab36aabe48259af87d64c59b6c
Christopher Hill <chill.signup@gmail.com>
2025-01-10 09:11:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ curl -s -o go "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/h
 
 # if the udev/rules.d directory doesn't exist, create it
 [ -d custom_scripts ] || mkdir -p custom_scripts
-curl -s -o custom_scripts/get_hdd_info.sh "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/custom_scripts_get_hdd_info.sh"
+curl -s -o custom_scripts/get_hdd_info.sh "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/custom_scripts/get_hdd_info.sh"
 cp /boot/config/custom_scripts/get_hdd_info.sh /usr/local/bin/
 chmod +x /usr/local/bin/get_hdd_info.sh
 
@@ -14,4 +14,3 @@ chmod +x /usr/local/bin/get_hdd_info.sh
 curl -s -o rules.d/60-persistent-storage.rules "https://raw.githubusercontent.com/chill-uk/unraid-uasp-fix/refs/heads/main/boot/config/rules.d/60-persistent-storage.rules"
 cp /boot/config/rules.d/60-persistent-storage.rules /etc/udev/rules.d/
 chmod 644 /etc/udev/rules.d/60-persistent-storage.rules
-
